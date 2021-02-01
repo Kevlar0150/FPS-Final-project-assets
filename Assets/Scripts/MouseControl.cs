9eb4da832e44b4658dd43544a1d5563129812998
Kevlar0150 <46628649+Kevlar0150@users.noreply.github.com>
2021-02-01 04:54:14 +0000

    Initial Commit
    - Has Movement controls and camera controls

---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    public float mouseSens = 350f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Removes cursor from the game.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXAxis = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime; // Gets mouse position on X axis
+        float mouseYAxis = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime; // Gets mouse position on Y axis
+
+        xRotation -= mouseYAxis; 
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamps rotation so can't look behind the character.
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //Apply rotation.
+        playerBody.Rotate(Vector3.up * mouseXAxis); //Allows player to rotate on X axis.
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 13.8f;
+
+    Vector3 velocity;
+    public float gravity = -18.81f;
+    public float jumpForce = 2.8f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Create a small physics sphere and checks collision with sphere and any Layers set to groundMask Layer and returns true or false
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); 
+
+        if (isGrounded && velocity.y < 0) // Sets character y position to 0 so character doesn't fall through ground
+        {
+            //Debug.Log("Grounded");
+            velocity.y = 0f;
+        }
+
+        // Gets values from Input Axis of Horizontal and Vertical (Default keys are WASD or Arrow Keys)
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+
+        controller.Move(movement * speed * Time.deltaTime); // moves the GameObject X and Y value in the given direction multiplied by speed value
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // If Space bar is pressed and character IS grounded
+        {
+            //Debug.Log("Jump");
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity); // Increase the players y velocity by Square root of jump height *-2 * gravity. ( Formula Taken from Unity Documentation )
+        }
+
+        velocity.y += gravity * Time.deltaTime; // Allows character position y to be manipulated by gravity
+
+        controller.Move(velocity * Time.deltaTime); // // moves the GameObject in all XYZ values in the given direction 
+    }
+}
