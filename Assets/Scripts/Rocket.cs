83560970218996cfc87737345a9aa43fbe6ff198
Kevlar0150 <46628649+Kevlar0150@users.noreply.github.com>
2021-02-13 18:12:04 +0000

    Added GunPack, VFXPack and PRogressed with shooting a rocket type gun
    -Added GunPack
    -Added VFXPack
    -Created a rocket type gun which uses physics detection for shooting.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed = 15f;
+    public float lifeDuration = 2f;
+
+    private float lifeTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimer = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make bullet move forward
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        //Check if bullet needs to be destroyed to save memory.
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0f)
+        {
+            DestroyObject(gameObject);
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If we want collision to occur when specified object is hit add "if (collision.gameObject.tag == "Something")"
+        DestroyObject(gameObject);
+    }
+}
---
+++ Assets/Scripts/RocketLauncher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour
+{
+    public Camera playerCamera;
+    public GameObject muzzlePosition;
+    public GameObject bulletPrefab;
+    public int shootInterval = 69;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            shootInterval++; // Increases shoot interval so there requires time between each shot (Can't just hold it down like a machine gun)
+            if (shootInterval >= 75)
+            {
+                GameObject bulletObject = Instantiate(bulletPrefab); // Instantiate bullet mesh being passed in
+                bulletObject.transform.position = muzzlePosition.transform.position + playerCamera.transform.forward; // Set position of the bullet
+                bulletObject.transform.forward = playerCamera.transform.forward; // Set facing direction of the bullet
+                Debug.Log("Fire!"); // Debug log to see if this If statement works.
+                shootInterval = 0;
+            }
+        }
+    }
+}
