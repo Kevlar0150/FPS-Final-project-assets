6157268d1735e22d5639be9eab42468dbce5b4d7
Kevlar0150 <46628649+Kevlar0150@users.noreply.github.com>
2021-02-19 19:50:14 +0000

    Start on Basic Enemy AI, Added NavMeshAgent and NavMeshSurface
    -Added enemy patrol,chase and attack using Unity.AI and NavMesh Components.
    
    TODO:
    Enemy shooting, stopping when in attack range and flying enemies need to be in the air.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,105 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public float enemyHealth = 50f;
+
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask Ground, Player;
+
+    // Patrol variables
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attack variables
+    public float timeBetweekAttacks;
+    bool hasAttacked;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
+        playerInSightRange = Physics.CheckSphere(transform.position, attackRange, Player);
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            Patrol(); // Patrols between points
+        }
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            Chase(); // Chases player until player is in attack range
+        }
+        if (playerInSightRange && playerInAttackRange)
+        {
+            Attack(); // Attacks player when in attack range
+        }
+    }
+
+    private void Patrol()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);        
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1.0f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random points in range
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 5f, Ground))
+        {
+            walkPointSet = true;
+        }
+    }
+    private void Chase()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void Attack()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!hasAttacked)
+        {
+            hasAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweekAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        hasAttacked = false;
     }
     public void TakeDamage(float damage)
     {
@@ -23,5 +109,13 @@ public class Enemy : MonoBehaviour
         {
             DestroyObject(gameObject); // Destroys the object that this script is attached too that has enemy health <= 0
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
