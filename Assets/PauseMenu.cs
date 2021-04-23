260a65946953a446a5836f562b4caaf982d53e6e
Kevlar0150 <46628649+Kevlar0150@users.noreply.github.com>
2021-04-23 19:30:04 +0100

    Pause Menu for nonVR mode
    Added Pause menu script for pause menu canvas in non vr mode

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool hasPaused = false;
+    public static bool hasControlInstructionPressed = false;
+    public GameObject pauseMenuUI;
+    public GameObject ControlInstructionPanel;
+    public GameObject HUD;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+            if (hasPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        HUD.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        hasPaused = false;
+    }
+
+    public void ControlsAndInstructions()
+    {
+        pauseMenuUI.SetActive(false);
+        ControlInstructionPanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        ControlInstructionPanel.SetActive(false);
+        PauseGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void PauseGame()
+    {
+        Cursor.lockState = CursorLockMode.None; // Removes cursor from the game.
+        HUD.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        hasPaused = true;
+    }
+}
